2ece033f3bf6ac2067833514d3b11a81efb97373
BrentWeiffenbach <rweiff2022@gmail.com>
2024-10-01 14:55:52 -0400

    Created real_time_yolo_demo.py to show off a demo of yolov10n, and can be used with trained model of groceries found in the run directory. Also added a README to explain how to install dependencies and get everything running

---
+++ Identification/README.txt
@@ -0,0 +1,14 @@
+Setting up identification:
+1. upgrade pip if needed:
+python.exe -m pip install --upgrade pip
+
+2. Install ultralytics:
+pip install ultralytics
+
+3. Install YOLOv10 dependencies:
+cd ./yolov10
+pip install .
+
+4. make sure ultralytics is at ultralytics-8.3.2
+pip show ultralytics
+pip install ultralytics --upgrade
---
+++ Identification/real_time_yolo_demo.py
@@ -0,0 +1,36 @@
+import cv2
+
+from ultralytics import YOLO
+
+# Load the YOLO model
+# model = YOLO("runs/detect/train/weights/best.pt") # test trained on fierburg grocery data set
+model = YOLO("yolov10n.pt")
+
+# capture video
+cap = cv2.VideoCapture(0)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO inference on the frame
+        results = model(frame)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLO Inference", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
